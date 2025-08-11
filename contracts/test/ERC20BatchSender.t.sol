f10d607410a8daf581b543d4a376fa7bfd57d7e8
Eunum56 <msiaffu123@gmail.com>
2025-08-11 11:16:36 +0530

    deploy: ERC20BatchSender on sepolia

--- contracts/test/ERC20BatchSender.t.sol
+++ contracts/test/ERC20BatchSender.t.sol
@@ -5,9 +5,11 @@ import {Test} from "forge-std/Test.sol";
 import {ERC20BatchSenderScript, ERC20BatchSender} from "../script/ERC20BatchSender.s.sol";
 
 contract ERC20BatchSenderTest is Test {
+    ERC20BatchSenderScript deployer;
     ERC20BatchSender erc20;
 
     function setUp() public {
-        erc20 = new ERC20BatchSenderScript();
+        deployer = new ERC20BatchSenderScript();
+        erc20 = deployer.run();
     }
 }
